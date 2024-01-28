9b39873c4e82172a85f3f01d4fb55dc3a68ca4ba
esad42 <aedhacettepe@gmail.com>
2024-01-29 01:49:09 +0300

    Empty Gameboard has built

--- src/components/gameboard/gameboard.js
+++ src/components/gameboard/gameboard.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './gameboard.css';
 
-const GameBoard = () => {
+const createBoard = () => {
+    return Array.from({length:20}, ()=> Array(10).fill(0));
 }
+const GameBoard = () => {
+    const [board,setBoard] = useState(createBoard);
+
+return <div className={"gameboard"}>
+    {board.map((row,rowIndex) => (<div key={rowIndex} className={"row"}>
+        {row.map((cell,cellIndex) => <div key={cellIndex} className={ cell === 0 ? "empty":"filled"}></div>)}
+    </div>))}
+</div>;
+}
+
 export default GameBoard;
