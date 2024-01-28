5799f7ba9b7e7b0f5a757452d9ff3d966086f276
esad42 <aedhacettepe@gmail.com>
2024-01-28 23:31:39 +0300

    new start

--- src/components/gameboard/gameboard.js
+++ src/components/gameboard/gameboard.js
@@ -2,26 +2,5 @@ import React from 'react';
 import './gameboard.css';
 
 const GameBoard = () => {
-    const boardWidth = 12;
-    const boardHeight = 22;
-    const totalCells = boardWidth * boardHeight;
-
-    // Hücreleri oluşturmak için bir dizi yarat
-    const cells = Array.from({ length: totalCells }, (_, index) => (
-        <Cell
-            key={index}
-            edge={
-                index < boardWidth ||
-                index % boardWidth === 0 ||
-                index % boardWidth === boardWidth - 1 ||
-                index >= boardWidth * (boardHeight - 1)
-            }
-        />
-    ));
-
-    return <div className="game-board">{cells}</div>;
-};
-
-const Cell = ({edge}) => <div className={`cell ${edge ? 'edge-cell' : ''}`}></div>;
-
+}
 export default GameBoard;
