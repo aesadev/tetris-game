e0aa06852227cb0e21789442a469fb342cb1fdd0
aesadev <ahmetesatduru@gmail.com>
2025-01-20 18:11:16 +0300

    adds hi

--- src/components/welcome-screen/welcome-screen.js
+++ src/components/welcome-screen/welcome-screen.js
@@ -7,7 +7,7 @@ const WelcomeScreen = ({ setActiveComponent }) => {
 		<div className="welcome-screen">
 			<h1>Welcome to Tetris</h1>
 			<div className="buttons">
-				<button onClick={() => setActiveComponent('GameplayScreen')}>Play</button>
+				<button onClick={() => setActiveComponent('GameplayScreen')}>Hi Play</button>
 				<button onClick={() => alert('Help information goes here.')}>Help</button>
 			</div>
 		</div>
